a40e113e594ed487ecc7d2554f04614b1bb00f3c
Amer <amerk86@gmail.com>
2021-05-06 21:50:36 +1000

    Text updates added survey

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -7,24 +7,22 @@
         <h1
           class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"
         >
-          <span class="block xl:inline">شكراً جزيلاً لاهتمامك.</span>
-          <span class="block text-orange-600 xl:inline">
-            نعتذر، التطبيق لم يجهز بعد.</span
-          >
+          <span class="block">يسعدنا انضمامك لعائلة توكيدات!</span>
+          <span class="block text-orange-600">نعتذر لعدم جاهزيتنا بعد.</span>
         </h1>
         <p
           class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
         >
-          سنرسل لك عند جاهزية التطبيق. وفي هذه الأثناء يمكنك الاستمتاع بتوكيدات
-          إيجابية على صفحتنا بالإنستغرام.
+          سنعلمك فور انطلاق التطبيق. يسرنا إن خصصت لنا من وقتك الثمين دقائق
+          معدودة للإجابة على بعض الأسئلة
         </p>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-          <div class="rounded-md shadow">
+          <div class="rounded-md shadow md:w-72">
             <a
-              href="https://www.instagram.com/arabicaffirmations/"
+              href="https://docs.google.com/forms/d/e/1FAIpQLScDqdP1J7HiOgEqllhlz8u8xtWUFRCu0eBFh9ZOsIBiMpBD5Q/viewform?vc=0&c=0&w=1&flr=0&gxids=7628&fbzx=-1156015142103195934"
               class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-orange-600 border border-transparent rounded-md hover:bg-orange-700 md:py-4 md:text-lg md:px-10"
             >
-              تابعنا على الإنستغرام
+              أجب أسئلة عن التوكيدات
             </a>
           </div>
           <!-- <div class="mt-3 sm:mt-0 sm:mr-3">
