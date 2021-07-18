9b3b7c5b2d5dce5752836189a2f1092ed84934a0
Amer <amerk86@gmail.com>
2021-07-18 13:04:10 +1000

    Added Danfoush to logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,4 +1,9 @@
 <template>
-  <div class="text-3xl font-black text-blue-500">توكيدات</div>
+  <div class="flex items-center justify-center">
+    <img class="w-9" src="logo.png" alt="توكيدات" />
+    <div class="mr-3 text-3xl font-black text-blue-500 h-3/5 md:h-2/5">
+      توكيدات
+    </div>
+  </div>
 </template>
 
